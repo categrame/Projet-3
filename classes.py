ebd831e7dae18b8d89fc49c02899a75615b21324
categrame <32358899+categrame@users.noreply.github.com>
2017-10-30 23:38:50 +0100

    Phase 2
    Added McGyver and moves him trought the labyrinth and win when he comes to guard.

--- classes.py
+++ classes.py
@@ -29,7 +29,7 @@ class Level:
 
     wall = pygame.image.load("img/wall.png").convert()
     start = pygame.image.load("img/start.png").convert()
-    final = pygame.image.load("img/final.png").convert()
+    final = pygame.image.load("img/guard.png").convert()
 
     line_number = 0
     for line in self.building:
@@ -45,3 +45,49 @@ class Level:
           window.blit(final,(x,y))
         case_number += 1
       line_number += 1              
+
+
+class Char:
+
+  def __init__(self,level):
+
+
+    self.image = pygame.image.load('img/mac.png').convert_alpha()
+
+    self.case_x = 0
+    self.case_y = 0
+    self.x = 0
+    self.y = 0
+    self.direction = self.image
+    self.level = level
+
+
+  def move(self, direction):
+
+    if direction == 'right':
+      if self.case_x < (number_of_sprite - 1):
+        if self.level.building[self.case_y][self.case_x+1] != 'w':
+          self.case_x += 1
+          self.x = self.case_x * sprite_size
+      self.direction = self.image
+
+    if direction == 'left':
+      if self.case_x > 0:
+        if self.level.building[self.case_y][self.case_x-1] != 'w':
+          self.case_x -= 1
+          self.x = self.case_x * sprite_size
+      self.direction = self.image
+
+    if direction == 'up':
+      if self.case_y > 0:
+        if self.level.building[self.case_y-1][self.case_x] != 'w':
+          self.case_y -= 1
+          self.y = self.case_y * sprite_size
+      self.direction = self.image
+
+    if direction == 'down':
+      if self.case_y < (number_of_sprite - 1):
+        if self.level.building[self.case_y+1][self.case_x] != 'w':
+          self.case_y += 1
+          self.y = self.case_y*sprite_size
+      self.direction = self.image
--- mcgyver.py
+++ mcgyver.py
@@ -9,6 +9,7 @@ pygame.init()
 window = pygame.display.set_mode((window_sprite,window_sprite))
 
 loop = 1
+game_loop = 1
 while loop:
 
   background = pygame.image.load("img/background.jpg").convert()
@@ -18,4 +19,28 @@ while loop:
   level.generate()
   level.display(window)
 
-  pygame.display.flip()
+  mcgyver = Char(level)
+
+  
+  while game_loop:
+    pygame.time.Clock().tick(30)
+    for event in pygame.event.get():
+    
+      if event.type == KEYDOWN:
+        if event.key == K_RIGHT:
+          mcgyver.move('right')
+        elif event.key == K_LEFT:
+          mcgyver.move('left')
+        elif event.key == K_UP:
+          mcgyver.move('up')
+        elif event.key == K_DOWN:
+          mcgyver.move('down')      
+
+    window.blit(background, (0,0))
+    level.display(window)
+    window.blit(mcgyver.direction, (mcgyver.x, mcgyver.y))
+    pygame.display.flip()
+
+    if level.building[mcgyver.case_y][mcgyver.case_x] == 'g':
+      pygame.display.quit()
+      pygame.quit()
